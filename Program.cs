25a698b8741abb308f8bf6baddb45d8bc2124c84
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-08-31 12:26:29 -0600

    :construction_worker: Se agregó el constructor de la clase libro y el manejo de propiedades opcionales

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Biblioteca
+﻿
+using Biblioteca.Clases;
+
+namespace Biblioteca
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenido a la biblioteca");
+
+            Libro unMundoFeliz = new Libro("Un mundo feliz", "Aldous Huxley"); 
+
         }
     }
 }
