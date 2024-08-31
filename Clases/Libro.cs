32ff69482a77da6d84b0640b4a9b0162ecb02792
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-08-31 12:14:27 -0600

    Add project files.

---
+++ Clases/Libro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Clases
+{
+    class Libro
+    {   
+        public string Titulo {get; set; }
+    }
+}
