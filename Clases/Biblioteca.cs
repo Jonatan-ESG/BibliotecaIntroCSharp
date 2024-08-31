21387413f38f8e57f61ade886018c177b69c38f6
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-08-31 12:37:52 -0600

    :construction_worker: Se agregó la clase Biblioteca

---
+++ Clases/Biblioteca.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Clases
+{
+    class MiBiblioteca
+    {
+        private List<Libro> Libros;
+
+        public MiBiblioteca()
+        {
+            Libros = new List<Libro>();
+        }
+
+        public void AgregarLibro(Libro libro)
+        {
+            Libros.Add(libro);
+            Console.WriteLine($"Libro {libro.Titulo} agregado a la biblioteca");
+        }
+
+        public void AgregarLibro(string titulo, string autor, int? anioPublicacion = null, string? isbn = null)
+        {
+            if(anioPublicacion.HasValue && isbn != null)
+            {
+                Libro nuevoLibro = new Libro(titulo, autor, anioPublicacion, isbn);
+                Libros.Add(nuevoLibro);
+            }
+            else if(anioPublicacion.HasValue)
+            {
+                Libro nuevoLibro = new Libro(titulo, autor, anioPublicacion);
+                Libros.Add(nuevoLibro);
+            }
+            else if(isbn != null)
+            {
+                Libro nuevoLibro = new Libro(titulo, autor, isbn: isbn);
+                Libros.Add(nuevoLibro);
+            }
+            else
+            {
+                Libro nuevoLibro = new Libro(titulo, autor);
+                Libros.Add(nuevoLibro);
+            }
+
+        }
+    }
+}
